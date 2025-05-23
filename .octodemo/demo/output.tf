e990da528b6df389022a0baf3719487763543d13
Vinicius Assuncao <viniciusvmda@github.com>
2025-05-23 10:20:56 -0300

    fix: clear demo creation notification body

--- .octodemo/demo/output.tf
+++ .octodemo/demo/output.tf
@@ -1,7 +1,7 @@
 output "demo_creation_output" {
   value = jsonencode({
     demo_entrypoint_url = jsondecode(module.repository.output_json).repo_url
-    notification_body = "Notification body example"
+    notification_body = ""
     custom_outputs = jsondecode(module.repository.output_json),
   })
 }
