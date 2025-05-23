aa48b06bd698faa939e4e554df4c3d4d97619bc6
Vinicius Assuncao <viniciusvmda@github.com>
2025-05-23 11:14:52 -0300

    chore: remove notification_body output

--- .octodemo/demo/output.tf
+++ .octodemo/demo/output.tf
@@ -1,7 +1,6 @@
 output "demo_creation_output" {
   value = jsonencode({
     demo_entrypoint_url = jsondecode(module.repository.output_json).repo_url
-    notification_body = ""
     custom_outputs = jsondecode(module.repository.output_json),
   })
 }
